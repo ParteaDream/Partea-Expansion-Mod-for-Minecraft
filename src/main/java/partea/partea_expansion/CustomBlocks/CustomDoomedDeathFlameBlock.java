42d66148d40ef354081c21cfb9f3155003c4b1cd
Stargomia <1378343195@qq.com>
2024-02-25 22:52:20 +0800

    1.0-Snapshot 6 Enchantment Update

---
+++ src/main/java/partea/partea_expansion/CustomBlocks/CustomDoomedDeathFlameBlock.java
@@ -0,0 +1,10 @@
+package partea.partea_expansion.CustomBlocks;
+
+import net.minecraft.block.FireBlock;
+
+public class CustomDoomedDeathFlameBlock extends FireBlock {
+    public CustomDoomedDeathFlameBlock(Settings settings) {
+        super(settings);
+    }
+
+}
